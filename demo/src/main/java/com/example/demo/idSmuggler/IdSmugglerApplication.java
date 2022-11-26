29309ea5a2f1de336c3c52b3b14f3b7a48643e6e
maslenchenko <o.maslenchenko@ucu.edu.ua>
2022-11-26 20:41:09 +0200

    adding demo folder

---
+++ demo/src/main/java/com/example/demo/idSmuggler/IdSmugglerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.idSmuggler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdSmugglerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdSmugglerApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/example/demo/idSmuggler/IdSmugglerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.idSmuggler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdSmugglerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
